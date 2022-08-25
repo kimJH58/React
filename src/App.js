aff29dc3411daa2c1d5ce5ba5cdded1ee34a36f9
jhJoyKim <kr.jihye.kim@gmail.com>
2022-08-25 22:07:46 +0900

    Link - to

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import {useState} from 'react';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App(){
   const [title, setTitle] = useState('');
@@ -12,35 +18,57 @@ function App(){
     })
   }
   return (
-    <div className="container">
-      <div className="mb-3">
-        <label className="form-label">Title</label>
-        <input 
-          className="form-control" 
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value)
-          }}
-        />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Body</label>
-        <textarea
-          className="form-control" 
-          value={body}
-          rows="20"
-          onChange={(e) => {
-            setBody(e.target.value)
-          }}
-        />
-      </div>
-      <button
-        onClick={onSubmit} 
-        className="btn btn-primary"
-        >
-          Post
-        </button>
-    </div>
+    <Router>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+          <Link class="navbar-brand" to="/">Home</Link>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link class="nav-link active" aria-current="page" to="/blogs">
+                Blogs
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/" exact>
+          Home Page
+        </Route>
+        <Route path="/blogs" exact>
+          <div className="container">
+            <h2>Create a blog post</h2>
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input 
+                className="form-control" 
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value)
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Body</label>
+              <textarea
+                className="form-control" 
+                value={body}
+                rows="20"
+                onChange={(e) => {
+                  setBody(e.target.value)
+                }}
+              />
+            </div>
+            <button
+                onClick={onSubmit} 
+                className="btn btn-primary"
+                >
+                Post
+            </button>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
