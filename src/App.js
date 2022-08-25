3f7237b0c92161acf3fcf7cedde72fe90f309d23
jhJoyKim <kr.jihye.kim@gmail.com>
2022-08-25 21:38:09 +0900

    insert data (with json-server)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import {useState} from 'react';
+import axios from 'axios';
+
 function App(){
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const onSubmit = () =>{
     console.log(title, body)
+    axios.post('http://localhost:3001/posts', {
+      title: title,
+      body: body
+    })
   }
   return (
     <div className="container">
