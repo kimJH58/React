d55e2b6577d5ea0dd5e0fda7b8b824dc265f02d9
jhJoyKim <kr.jihye.kim@gmail.com>
2022-09-30 20:18:44 +0900

    LoadingSpinner

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -4,21 +4,72 @@ import {useState, useEffect} from 'react';
 import Card from '../components/Card';
 import { Link } from 'react-router-dom';
 import {useHistory} from 'react-router';
-
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const ListPage = () =>{
     const history = useHistory();
     const [posts, setPosts] = useState([]);
-
+    const [loading, setLoading] = useState(true);
     const getPosts = () => {
         axios.get('http://localhost:3002/api/getPostList').then((res) =>{
             setPosts(res.data);
+            setLoading(false);
         })
     }
 
     useEffect(() => {
         getPosts();
     }, []);
+
+    const renderBlogList = () =>{
+        if(loading){
+            return(
+                <LoadingSpinner />
+            );
+        }
+
+        if(posts.length === 0){
+            return(
+                <div>
+                    'No blog posts found'
+                </div>
+            )
+        }
+
+        return posts.map(post =>{
+            return (
+                <Card 
+                    key = {post.id} 
+                    title={post.title}
+                    onClick={()=>{history.push({
+                        pathname : '/blogs/read',
+                        state : {
+                            post:post
+                        }
+                    })}}
+                >
+                    <button 
+                        className='btn btn-success btn-sm'
+                        onClick={(event) => {
+                            event.stopPropagation();
+                            history.push({
+                                pathname : '/blogs/edit',
+                                state : {
+                                    id : post.id
+                                }
+                            })
+                        }}
+                    >
+                        edit
+                    </button>
+                    {/* 한개의 element만 가능 */}
+                    {/* <button>
+                        delete
+                    </button> */}
+                </Card>
+            )
+        })
+    }
 
     return (
         <div>
@@ -30,39 +81,7 @@ const ListPage = () =>{
                     </Link>
                 </div>
             </div>
-            {posts.map(post =>{
-                return (
-                    <Card 
-                        key = {post.id} 
-                        title={post.title}
-                        onClick={()=>{history.push({
-                            pathname : '/blogs/read',
-                            state : {
-                                post:post
-                            }
-                        })}}
-                    >
-                        <button 
-                            className='btn btn-success btn-sm'
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                history.push({
-                                    pathname : '/blogs/edit',
-                                    state : {
-                                        id : post.id
-                                    }
-                                })
-                            }}
-                        >
-                            edit
-                        </button>
-                        {/* 한개의 element만 가능 */}
-                        {/* <button>
-                            delete
-                        </button> */}
-                    </Card>
-                )
-            })}
+            {renderBlogList()}
         </div>
     );
 };
