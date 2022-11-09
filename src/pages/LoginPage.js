710b963a01d0ff31391a5168d36a347433477f70
jhJoyKim <kr.jihye.kim@gmail.com>
2022-11-09 23:19:24 +0900

    css) loginPage

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,11 +3,28 @@ import React from "react";
 const ReadPage = () =>{
     return (
         <div>
-            <title>Login</title>
-            <div className="mb-3">
-                <label for="exampleFormControlInput1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-            </div>
+            <h4 className="text-center">Login</h4>
+            <form>
+                <div className="row mb-3">
+                    <label for="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+                    <div className="col-sm-10">
+                        <input type="email" className="form-control" id="inputEmail3"/>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+                    <div className="col-sm-10">
+                        <input type="password" className="form-control" id="inputPassword3"/>
+                    </div>
+                </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button
+                        type="submit" className="btn btn-primary"
+                    >
+                        Login
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
